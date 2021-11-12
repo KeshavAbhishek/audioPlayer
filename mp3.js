dda4229b7c6d8ac8088b0ccbde0670716657d104
Cool_Tkinter_Projects <crystaled2003@gmail.com>
2021-11-13 00:46:38 +0530

    Audio Player Assets

---
+++ mp3.js
@@ -0,0 +1,50 @@
+var on = 1;
+function mute(){
+    if(on==1){
+        document.getElementById('unmuted').setAttribute('src','https://keshavabhishek.github.io/web/mute.jpg');
+        on = 0;
+    }
+    else if(on==0){
+        document.getElementById('unmuted').setAttribute('src','https://cdn.iconscout.com/icon/free/png-256/unmute-433180.png');
+        on = 1;
+    };
+};
+
+var pause = 0;
+function pasueunpauseSong(){
+    if(pause==1){
+        document.getElementById('playpausebutton').setAttribute('src','play.png');
+        pause=0;
+    }
+    else if(pause==0){
+        document.getElementById('playpausebutton').setAttribute('src','pause.png');
+        pause=1;
+    };
+};
+
+function playme(){
+    var song=document.getElementById('audioPlayerControlsBar');
+    song.setAttribute('src',document.getElementById('addSongOpenFile').files[0].name);
+};
+
+function selectSong_and_Play(){
+    var openfile = document.getElementById('addSongOpenFile').click();
+};
+
+document.getElementById('addSongOpenFile').addEventListener('change',()=>{
+    if(document.getElementById('addSongOpenFile').files[0].name!=''){
+        playme();
+    }
+});
+
+document.getElementById('loadSong').addEventListener('mousedown',()=>{
+    document.getElementById('loadSong').style.transform='translateX(0.15em)';
+    document.getElementById('loadSong').style.transform='translateY(0.15em)';
+    document.getElementById('loadSong').style.boxShadow='none';
+});
+
+document.getElementById('loadSong').addEventListener('mouseup',()=>{
+    document.getElementById('loadSong').style.transform='translateX(-0.15em)';
+    document.getElementById('loadSong').style.transform='translateY(-0.15em)';
+    document.getElementById('loadSong').style.boxShadow='0.15em 0.15em 0em #000';
+});
